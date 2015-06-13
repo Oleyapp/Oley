3f7ec4d98267a381f98c482f073ddad1acb553fb
Faiz Mokhtar <mfmokhtar@gmail.com>
2015-06-14 05:17:43 +0800

    Add OLBookView.

---
+++ Oleyh/OLBookView.h
@@ -0,0 +1,13 @@
+//
+//  OLBookView.h
+//  Oleyh
+//
+//  Created by faiz mokhtar on 6/14/15.
+//  Copyright (c) 2015 Faiz Mokhtar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OLBookView : UIView
+
+@end
---
+++ Oleyh/OLBookView.m
@@ -0,0 +1,21 @@
+//
+//  OLBookView.m
+//  Oleyh
+//
+//  Created by faiz mokhtar on 6/14/15.
+//  Copyright (c) 2015 Faiz Mokhtar. All rights reserved.
+//
+
+#import "OLBookView.h"
+
+@implementation OLBookView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
