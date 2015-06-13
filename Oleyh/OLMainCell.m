668545f663c7d27fd5b10efb83e61589aa497da4
Faiz Mokhtar <mfmokhtar@gmail.com>
2015-06-13 15:18:01 +0800

    Add Main controller and cell.

---
+++ Oleyh/OLMainCell.m
@@ -0,0 +1,23 @@
+//
+//  OLMainCell.m
+//  Oleyh
+//
+//  Created by faiz mokhtar on 6/13/15.
+//  Copyright (c) 2015 Faiz Mokhtar. All rights reserved.
+//
+
+#import "OLMainCell.h"
+
+@implementation OLMainCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
