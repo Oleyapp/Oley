e7e9193a2e0f7cc3db0264626acb36d9fe811e6c
Faiz Mokhtar <mfmokhtar@gmail.com>
2015-06-14 06:08:03 +0800

    Setup booking request.

--- Oleyh/OLBookView.m
+++ Oleyh/OLBookView.m
@@ -7,13 +7,24 @@
 //
 
 #import "OLBookView.h"
+#import "OLVenueController.h"
 
 @implementation OLBookView
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    
+    if ([viewController isKindOfClass:[OLVenueController class]]) {
+        self.hidden = NO;
+    } else {
+        self.hidden = YES;        
+    }
+}
 
 #pragma mark - IBActions
 
 - (IBAction)bookVenue:(id)sender {
     NSLog(@"Button pressed");
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"didBookVenue" object:nil];
 }
 
 @end
--- Oleyh/OLVenueController.m
+++ Oleyh/OLVenueController.m
@@ -10,6 +10,8 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "RMDateSelectionViewController.h"
 
+#import "AFNetworking.h"
+
 @interface OLVenueController ()
 
 @end
@@ -28,6 +30,9 @@
     self.taglineLabel.text = self.tagline;
 //    [self.backgroundImageView sd_setImageWithURL:[NSURL URLWithString:self.backgroundImageUrl] placeholderImage:nil];
 //    [self.logoImageView sd_setImageWithURL:[NSURL URLWithString:self.logoImageUrl] placeholderImage:nil];
+    
+    // Register notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(bookVenue) name:@"didBookVenue" object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,9 +45,9 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     NSLog(@"Index: %@", indexPath.description);
     
-    if (indexPath.row == 1) {
+    if (indexPath.row == 2) {
         [self openDateSelectionControllerWithIndex:indexPath.row];
-    } else if (indexPath.row == 2) {
+    } else if (indexPath.row == 4) {
         [self openDateSelectionControllerWithIndex:indexPath.row];
     }
 }
@@ -62,13 +67,15 @@
         
         NSDate *date = ((UIDatePicker *)controller.contentView).date;
         NSDateFormatter *dateFormatter =[[NSDateFormatter alloc]init];
-        [dateFormatter setDateFormat:@"yyyy"];
+        dateFormatter.dateStyle = NSDateFormatterFullStyle;
         NSString *dateString = [dateFormatter stringFromDate:date];
         
-        if (row == 1) {
+        NSLog(@"Row: %ld", (long)row);
+        
+        if (row == 2) {
             NSLog(@"Date start: %@", ((UIDatePicker *)controller.contentView).date);
             self.startLabel.text = dateString;
-        } else if (row == 2) {
+        } else if (row == 4) {
             self.endLabel.text = dateString;
         }
         
@@ -83,8 +90,8 @@
     
     if (row == 1) {
         dateSelectionController.title = @"When do you want to book it?";
-    } else {
-        dateSelectionController.title = @"Till when?";
+    } else if (row == 3){
+        dateSelectionController.title = @"until when?";
     }
     
     dateSelectionController.message = @"This is a test message.\nPlease choose a date and press 'Select' or 'Cancel'.";
@@ -92,11 +99,45 @@
     [self presentViewController:dateSelectionController animated:YES completion:nil];
 }
 
+#pragma mark - Methods
+
 - (void)customizeViews {
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(-36, 0, 0, 0);
     
     self.logoImageView.layer.cornerRadius = self.logoImageView.frame.size.width / 2;
     self.logoImageView.layer.borderWidth = 2;
     self.logoImageView.layer.borderColor = [UIColor whiteColor].CGColor;
+    
+    UINib *nib = [UINib nibWithNibName:@"OLBookView" bundle:nil];
+    UIView *bookView = [[nib instantiateWithOwner:self options:nil] objectAtIndex:0];
+    CGRect frame = bookView.frame;
+    frame.origin.y = [[UIScreen mainScreen] bounds].size.height - bookView.frame.size.height - 8;
+    frame.origin.x = 8;
+    frame.size.width = [[UIScreen mainScreen] bounds].size.width - 16;
+    [bookView setFrame:frame];
+    self.navigationController.delegate = bookView;
+    [self.navigationController.view addSubview:bookView];
+
+}
+
+- (void)bookVenue {
+    
+    NSDictionary *parameters = @{
+                                 @"start_at":@"",
+                                 @"end_at":@""
+                                 };
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager POST:[NSString stringWithFormat:@"http://192.168.2.1:8000/api/v1/book?token=%@", [defaults valueForKey:@"token"]]
+       parameters:parameters
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              NSLog(@"Response: %@", responseObject);
+          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              NSLog(@"Error: %@", error);
+          }];
 }
 
 @end
