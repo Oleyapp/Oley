49c56286da48b38ab3e982291ef28b879e7c4ab9
Faiz Mokhtar <mfmokhtar@gmail.com>
2015-06-13 16:25:12 +0800

    Add User model.

---
+++ Oleyh/OLUser.h
@@ -0,0 +1,13 @@
+//
+//  OLUser.h
+//  Oleyh
+//
+//  Created by faiz mokhtar on 6/13/15.
+//  Copyright (c) 2015 Faiz Mokhtar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OLUser : NSObject
+
+@end
---
+++ Oleyh/OLUser.m
@@ -0,0 +1,13 @@
+//
+//  OLUser.m
+//  Oleyh
+//
+//  Created by faiz mokhtar on 6/13/15.
+//  Copyright (c) 2015 Faiz Mokhtar. All rights reserved.
+//
+
+#import "OLUser.h"
+
+@implementation OLUser
+
+@end
