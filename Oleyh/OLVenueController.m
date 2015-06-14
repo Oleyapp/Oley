599abd05302d1d700b4d68820848c11e9bfcffcc
Faiz Mokhtar <mfmokhtar@gmail.com>
2015-06-14 09:27:51 +0800

    Load image and finish up booking request :sleepy:

--- Oleyh/OLVenueController.m
+++ Oleyh/OLVenueController.m
@@ -9,10 +9,15 @@
 #import "OLVenueController.h"
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "RMDateSelectionViewController.h"
-
+#import "CNPPopUpController.h"
 #import "AFNetworking.h"
 
 @interface OLVenueController ()
+
+@property (nonatomic, strong) CNPPopupController *popupController;
+
+@property (nonatomic, strong) NSDate *dateStart;
+@property (nonatomic, strong) NSDate *dateEnd;
 
 @end
 
@@ -28,8 +33,8 @@
     self.tableView.delegate = self;
     
     self.taglineLabel.text = self.tagline;
-//    [self.backgroundImageView sd_setImageWithURL:[NSURL URLWithString:self.backgroundImageUrl] placeholderImage:nil];
-//    [self.logoImageView sd_setImageWithURL:[NSURL URLWithString:self.logoImageUrl] placeholderImage:nil];
+    [self.backgroundImageView sd_setImageWithURL:[NSURL URLWithString:self.backgroundImageUrl] placeholderImage:nil];
+    [self.logoImageView sd_setImageWithURL:[NSURL URLWithString:self.logoImageUrl] placeholderImage:nil];
     
     // Register notifications
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(bookVenue) name:@"didBookVenue" object:nil];
@@ -75,8 +80,10 @@
         if (row == 2) {
             NSLog(@"Date start: %@", ((UIDatePicker *)controller.contentView).date);
             self.startLabel.text = dateString;
+            self.dateStart = date;
         } else if (row == 4) {
             self.endLabel.text = dateString;
+            self.dateEnd = date;
         }
         
     }];
@@ -121,22 +128,99 @@
 
 }
 
+- (void)showPopupSuccessWithStyle:(CNPPopupStyle)popupStyle {
+    
+    NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    
+    NSString *descriptionString = @"Your booking have been accepted. Be there on time. :)";
+
+    NSAttributedString *lineOne = [[NSAttributedString alloc] initWithString:descriptionString attributes:@{NSFontAttributeName : [UIFont fontWithName:@"Avenir-medium" size:18.0], NSParagraphStyleAttributeName : paragraphStyle}];
+    UIImage *icon = [UIImage imageNamed:@"IconSuccess"];
+    
+    NSAttributedString *buttonTitle2 = [[NSAttributedString alloc] initWithString:@"Yes" attributes:@{NSFontAttributeName : [UIFont fontWithName:@"Avenir-medium" size:18.0], NSForegroundColorAttributeName : [UIColor whiteColor], NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    CNPPopupButtonItem *buttonItem2 = [CNPPopupButtonItem defaultButtonItemWithTitle:buttonTitle2 backgroundColor:[UIColor colorWithRed:0.233 green:0.480 blue:0.858 alpha:1.000]];
+    buttonItem2.selectionHandler = ^(CNPPopupButtonItem *item){
+        NSLog(@"Block for button: %@", item.buttonTitle.string);
+    };
+    
+    self.popupController = [[CNPPopupController alloc] initWithTitle:nil contents:@[icon, lineOne]
+                                                         buttonItems:@[buttonItem2]
+                                               destructiveButtonItem:nil];
+    self.popupController.theme = [CNPPopupTheme defaultTheme];
+    
+    self.popupController.theme.contentVerticalPadding = 20.0f;
+    //    self.popupController.delegate = self;
+    self.popupController.theme.presentationStyle = CNPPopupPresentationStyleFadeIn;
+    [self.popupController presentPopupControllerAnimated:YES];
+}
+
+- (void)showPopupFailedWithStyle:(CNPPopupStyle)popupStyle {
+    
+    NSMutableParagraphStyle *paragraphStyle = NSMutableParagraphStyle.new;
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    
+    NSString *descriptionString = @"Aww shucks, it looks like there is no available spot. Try at another time.";
+    //
+    //    NSAttributedString *title = [[NSAttributedString alloc] initWithString:@"It's A Popup!" attributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:24], NSParagraphStyleAttributeName : paragraphStyle}];
+    NSAttributedString *lineOne = [[NSAttributedString alloc] initWithString:descriptionString attributes:@{NSFontAttributeName : [UIFont fontWithName:@"Avenir-medium" size:18.0], NSParagraphStyleAttributeName : paragraphStyle}];
+    UIImage *icon = [UIImage imageNamed:@"IconErrors"];
+    
+    NSAttributedString *buttonTitle2 = [[NSAttributedString alloc] initWithString:@"Okay" attributes:@{NSFontAttributeName : [UIFont fontWithName:@"Avenir-medium" size:18.0], NSForegroundColorAttributeName : [UIColor whiteColor], NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    CNPPopupButtonItem *buttonItem2 = [CNPPopupButtonItem defaultButtonItemWithTitle:buttonTitle2 backgroundColor:[UIColor colorWithRed:0.233 green:0.480 blue:0.858 alpha:1.000]];
+    buttonItem2.selectionHandler = ^(CNPPopupButtonItem *item){
+        NSLog(@"Block for button: %@", item.buttonTitle.string);
+    };
+    
+    self.popupController = [[CNPPopupController alloc] initWithTitle:nil contents:@[icon, lineOne]
+                                                         buttonItems:@[buttonItem2]
+                                               destructiveButtonItem:nil];
+    self.popupController.theme = [CNPPopupTheme defaultTheme];
+    
+    self.popupController.theme.contentVerticalPadding = 20.0f;
+    //    self.popupController.delegate = self;
+    self.popupController.theme.presentationStyle = CNPPopupPresentationStyleFadeIn;
+    [self.popupController presentPopupControllerAnimated:YES];
+}
+
+
+#pragma mark - NSNotifications
+
 - (void)bookVenue {
+
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"YYYY-MM-dd HH:mm:ss"];
+    
+    NSString *startDate = [dateFormatter stringFromDate:self.dateStart];
+    NSString *endDate = [dateFormatter stringFromDate:self.dateEnd];
     
     NSDictionary *parameters = @{
-                                 @"start_at":@"",
-                                 @"end_at":@""
+                                 @"court_id": self.venueId,
+                                 @"start_at":startDate,
+                                 @"end_at":endDate
                                  };
+    
+    NSLog(@"PArams: %@", parameters);
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    [manager POST:[NSString stringWithFormat:@"http://192.168.2.1:8000/api/v1/book?token=%@", [defaults valueForKey:@"token"]]
+    [manager POST:[NSString stringWithFormat:@"http://192.168.2.1:8000/api/player/v1/book?token=%@", [defaults valueForKey:@"token"]]
        parameters:parameters
           success:^(AFHTTPRequestOperation *operation, id responseObject) {
               NSLog(@"Response: %@", responseObject);
+              
+              [self showPopupSuccessWithStyle:CNPPopupStyleCentered];
+              
           } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
               NSLog(@"Error: %@", error);
+              [self showPopupFailedWithStyle:CNPPopupStyleCentered];
+
           }];
 }
 
